faf9638f07c368e7fe977ba431547cd1f767942a
Solomon Raj A <solomonraj.a@presidio.com>
2021-12-01 12:57:42 +0530

    stagging

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Hello, Django from App service!")
+    return HttpResponse("Hello, Django from App service! Staging")
